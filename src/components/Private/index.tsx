5461740ffee628209bf618f56c882569da000b3a
Henrqque <hdoido1992@gmail.com>
2024-02-16 21:30:54 -0300

    fix: fixed re-render to pages

--- src/components/Private/index.tsx
+++ src/components/Private/index.tsx
@@ -1,20 +1,12 @@
 "use client";
 import { AuthContext } from "@/context/auth/AuthContext";
-import { useRouter } from "next/navigation";
-import { ReactNode, useContext, useEffect } from "react";
+import { ReactNode, useContext } from "react";
 
 type PrivateProps = {
   children: ReactNode;
 };
 export const Private = ({ children }: PrivateProps) => {
-  const { push } = useRouter();
   const { isAuthenticated } = useContext(AuthContext);
-
-  useEffect(() => {
-    if (!isAuthenticated) {
-      push("/signin");
-    }
-  }, [isAuthenticated, push]);
 
   return (
     <>
